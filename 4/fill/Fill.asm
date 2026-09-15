9075d1e5086cb40464323eccd181220c14c1e7a5
bp019 <bp019@bucknell.edu>
2026-09-15 15:03:06 -0400

    Project 2&3 Completed

--- 4/fill/Fill.asm
+++ 4/fill/Fill.asm
@@ -8,4 +8,5 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+@KBD
+D = M
--- 4/mult/Mult.asm
+++ 4/mult/Mult.asm
@@ -7,4 +7,29 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+@R2
+M = 0
+
+(LOOP)
+@R0 // Load R0 Address
+D = M // Load R0 Data
+@END // Load Address for Jump
+D; JEQ // Jump if R1=0
+
+@R1 
+D = M // Load R1 Data
+@R2 
+M = D + M // Add R1 to R2
+
+@R0
+M = M - 1 // Decrement R0
+
+@LOOP
+0;JMP //Repeat Loop until R1=0
+
+(END)
+@END
+0; JMP // I can't believe this works!!!!!!!!
+
+
+
